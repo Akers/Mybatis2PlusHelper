04b6fc6c75578931ecc00228e52716c2a66818ce
GitHub Action <action@github.com>
2023-07-25 09:58:57 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "Mybatis2PlusHelper"
--- src/main/kotlin/com/github/akers/mybatis2plushelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/akers/mybatis2plushelper/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.akers.mybatis2plushelper.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.akers.mybatis2plushelper.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
